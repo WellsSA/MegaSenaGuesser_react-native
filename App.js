df723ce9bd8b24f5aebcea73afa77973a33786fe
WellsSA <wel.cavzod@gmail.com>
2020-04-07 07:07:51 -0300

    App.js: generateRandomNumbers functionality

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
+
 export default function App() {
-  const [outputText, setOutputText] = useState('Texto inicial');
+  const [randomNumbers, setRandomNumbers] = useState(Array(6).fill(0));
+
+  function generateRandomNumber() {
+    return Math.ceil(Math.random() * 60);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Alterar texto" onPress={() => setOutputText('Novo Texto')}/>
+      <Text>{randomNumbers.join(', ')}</Text>
+      <Button title="Gerar números" onPress={
+        () => setRandomNumbers(randomNumbers.map(() => generateRandomNumber()))
+      }/>
     </View>
   );
 }
